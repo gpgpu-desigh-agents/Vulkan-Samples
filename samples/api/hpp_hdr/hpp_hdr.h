456debcbfb21e1f32d09a6afc895051911e83324
johnson.chen <joy6677@qq.com>
2024-10-06 16:58:30 +0800

    Update content and rename

---
+++ samples/api/hpp_hdr/hpp_hdr.h
@@ -0,0 +1,230 @@
+/* Copyright (c) 2022-2023, NVIDIA CORPORATION. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * High dynamic range rendering, using vulkan.hpp
+ */
+
+#pragma once
+
+#include <hpp_api_vulkan_sample.h>
+
+class HPPHDR : public HPPApiVulkanSample
+{
+  public:
+	HPPHDR();
+	~HPPHDR();
+
+  private:
+	struct Bloom
+	{
+		bool                    enabled               = true;
+		vk::DescriptorSetLayout descriptor_set_layout = {};
+		vk::DescriptorSet       descriptor_set;
+		vk::PipelineLayout      pipeline_layout;
+		vk::Pipeline            pipelines[2];
+
+		void destroy(vk::Device device, vk::DescriptorPool descriptor_pool)
+		{
+			device.destroyPipeline(pipelines[0]);
+			device.destroyPipeline(pipelines[1]);
+			device.destroyPipelineLayout(pipeline_layout);
+			device.freeDescriptorSets(descriptor_pool, descriptor_set);
+			device.destroyDescriptorSetLayout(descriptor_set_layout);
+		}
+	};
+
+	struct Composition
+	{
+		vk::DescriptorSetLayout descriptor_set_layout = {};
+		vk::DescriptorSet       descriptor_set        = {};
+		vk::PipelineLayout      pipeline_layout       = {};
+		vk::Pipeline            pipeline              = {};
+
+		void destroy(vk::Device device, vk::DescriptorPool descriptor_pool)
+		{
+			device.destroyPipeline(pipeline);
+			device.destroyPipelineLayout(pipeline_layout);
+			device.freeDescriptorSets(descriptor_pool, descriptor_set);
+			device.destroyDescriptorSetLayout(descriptor_set_layout);
+		}
+	};
+
+	// Framebuffer for offscreen rendering
+	struct FrameBufferAttachment
+	{
+		vk::Format       format = {};
+		vk::Image        image  = {};
+		vk::DeviceMemory mem    = {};
+		vk::ImageView    view   = {};
+
+		void destroy(vk::Device device)
+		{
+			device.destroyImageView(view);
+			device.destroyImage(image);
+			device.freeMemory(mem);
+		}
+	};
+
+	struct FilterPass
+	{
+		vk::Extent2D          extent      = {};
+		vk::Framebuffer       framebuffer = {};
+		FrameBufferAttachment color       = {};
+		vk::RenderPass        render_pass = {};
+		vk::Sampler           sampler     = {};
+
+		void destroy(vk::Device device)
+		{
+			device.destroySampler(sampler);
+			device.destroyFramebuffer(framebuffer);
+			device.destroyRenderPass(render_pass);
+			color.destroy(device);
+		}
+	};
+
+	struct Geometry
+	{
+		vk::DescriptorSet                                                      descriptor_set = {};
+		vk::Pipeline                                                           pipeline       = {};
+		std::vector<std::unique_ptr<vkb::scene_graph::components::HPPSubMesh>> meshes         = {};
+
+		void destroy(vk::Device device, vk::DescriptorPool descriptor_pool)
+		{
+			device.freeDescriptorSets(descriptor_pool, descriptor_set);
+			device.destroyPipeline(pipeline);
+		}
+	};
+
+	struct Models
+	{
+		vk::DescriptorSetLayout descriptor_set_layout = {};
+		vk::PipelineLayout      pipeline_layout       = {};
+		Geometry                objects               = {};
+		Geometry                skybox                = {};
+		std::vector<glm::mat4>  transforms            = {};
+		int32_t                 object_index          = 0;
+
+		void destroy(vk::Device device, vk::DescriptorPool descriptor_pool)
+		{
+			objects.destroy(device, descriptor_pool);
+			skybox.destroy(device, descriptor_pool);
+			device.destroyPipelineLayout(pipeline_layout);
+			device.destroyDescriptorSetLayout(descriptor_set_layout);
+		}
+	};
+
+	struct Offscreen
+	{
+		vk::Extent2D          extent      = {};
+		vk::Framebuffer       framebuffer = {};
+		FrameBufferAttachment color[2]    = {};
+		FrameBufferAttachment depth       = {};
+		vk::RenderPass        render_pass = {};
+		vk::Sampler           sampler     = {};
+
+		void destroy(vk::Device device)
+		{
+			device.destroySampler(sampler);
+			device.destroyFramebuffer(framebuffer);
+			device.destroyRenderPass(render_pass);
+			color[0].destroy(device);
+			color[1].destroy(device);
+			depth.destroy(device);
+		}
+	};
+
+	struct Textures
+	{
+		HPPTexture envmap;
+
+		void destroy(vk::Device device)
+		{
+			device.destroySampler(envmap.sampler);
+		}
+	};
+
+	struct UBOMatrices
+	{
+		glm::mat4 projection;
+		glm::mat4 modelview;
+		glm::mat4 skybox_modelview;
+		float     modelscale = 0.05f;
+	};
+
+	struct UBOParams
+	{
+		float exposure = 1.0f;
+	};
+
+	struct UniformBuffers
+	{
+		std::unique_ptr<vkb::core::HPPBuffer> matrices;
+		std::unique_ptr<vkb::core::HPPBuffer> params;
+	};
+
+  private:
+	// from platform::HPPApplication
+	virtual bool prepare(vkb::platform::HPPPlatform &platform) override;
+	virtual bool resize(const uint32_t width, const uint32_t height) override;
+
+	// from HPPVulkanSample
+	virtual void request_gpu_features(vkb::core::HPPPhysicalDevice &gpu) override;
+
+	// from HPPApiVulkanSample
+	virtual void build_command_buffers() override;
+	virtual void on_update_ui_overlay(vkb::HPPDrawer &drawer) override;
+	virtual void render(float delta_time) override;
+
+	vk::DeviceMemory      allocate_memory(vk::Image image);
+	FrameBufferAttachment create_attachment(vk::Format format, vk::ImageUsageFlagBits usage);
+	void                  create_bloom_pipelines();
+	void                  create_composition_pipeline();
+	vk::Image             create_image(vk::Format format, vk::ImageUsageFlagBits usage);
+	vk::ImageView         create_image_view(vk::Format format, vk::ImageUsageFlagBits usage, vk::Image image);
+	void                  create_models_pipelines();
+	vk::RenderPass        create_render_pass(std::vector<vk::AttachmentDescription> const &attachment_descriptions, vk::SubpassDescription const &subpass_description);
+	vk::Sampler           create_sampler();
+	void                  draw();
+	void                  load_assets();
+	void                  prepare_camera();
+	void                  prepare_offscreen_buffer();
+	void                  prepare_uniform_buffers();
+	void                  setup_bloom();
+	void                  setup_composition();
+	void                  setup_models();
+	void                  update_composition_descriptor_set();
+	void                  update_bloom_descriptor_set();
+	void                  update_model_descriptor_set(vk::DescriptorSet descriptor_set);
+	void                  update_params();
+	void                  update_uniform_buffers();
+
+  private:
+	Bloom                    bloom;
+	Composition              composition;
+	bool                     display_skybox = true;
+	FilterPass               filter_pass;
+	Models                   models;
+	std::vector<std::string> object_names;
+	Offscreen                offscreen;
+	Textures                 textures;
+	UBOMatrices              ubo_matrices;
+	UBOParams                ubo_params;
+	UniformBuffers           uniform_buffers;
+};
+
+std::unique_ptr<vkb::Application> create_hpp_hdr();
---
+++ samples/api/hpp_hello_triangle/hpp_hello_triangle.h
@@ -0,0 +1,96 @@
+/* Copyright (c) 2021-2023, NVIDIA CORPORATION. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <platform/hpp_application.h>
+#include <platform/hpp_platform.h>
+
+/**
+ * @brief A self-contained (minimal use of framework) sample that illustrates
+ * the rendering of a triangle
+ */
+class HPPHelloTriangle : public vkb::platform::HPPApplication
+{
+	/**
+	 * @brief Swapchain data
+	 */
+	struct SwapchainData
+	{
+		vk::Extent2D                 extent;                                 // The swapchain extent
+		vk::Format                   format = vk::Format::eUndefined;        // Pixel format of the swapchain.
+		vk::SwapchainKHR             swapchain;                              // The swapchain.
+		std::vector<vk::ImageView>   image_views;                            // The image view for each swapchain image.
+		std::vector<vk::Framebuffer> framebuffers;                           // The framebuffer for each swapchain image view.
+	};
+
+	/**
+	 * @brief Per-frame data
+	 */
+	struct FrameData
+	{
+		vk::Fence         queue_submit_fence;
+		vk::CommandPool   primary_command_pool;
+		vk::CommandBuffer primary_command_buffer;
+		vk::Semaphore     swapchain_acquire_semaphore;
+		vk::Semaphore     swapchain_release_semaphore;
+	};
+
+  public:
+	HPPHelloTriangle();
+	virtual ~HPPHelloTriangle();
+
+  private:
+	// from platform::HPPApplication
+	virtual bool prepare(vkb::platform::HPPPlatform &platform) override;
+	virtual void update(float delta_time) override;
+	virtual bool resize(const uint32_t width, const uint32_t height) override;
+
+	std::pair<vk::Result, uint32_t> acquire_next_image();
+	void                            init_device(const std::vector<const char *> &required_device_extensions);
+	void                            init_frame(size_t frame);
+	void                            init_framebuffers();
+	void                            init_instance(const std::vector<const char *> &required_instance_extensions,
+	                                              const std::vector<const char *> &required_validation_layers);
+	void                            init_pipeline();
+	void                            init_swapchain();
+	void                            render_triangle(uint32_t swapchain_index);
+	void                            select_physical_device_and_surface(vkb::platform::HPPPlatform &platform);
+	void                            teardown_framebuffers();
+	void                            teardown_per_frame(FrameData &per_frame_data);
+
+  private:
+	vk::Instance               instance;                     // The Vulkan instance.
+	vk::PhysicalDevice         gpu;                          // The Vulkan physical device.
+	vk::Device                 device;                       // The Vulkan device.
+	vk::Queue                  queue;                        // The Vulkan device queue.
+	SwapchainData              swapchain_data;               // The swapchain state.
+	vk::SurfaceKHR             surface;                      // The surface we will render to.
+	uint32_t                   graphics_queue_index;         // The queue family index where graphics work will be submitted.
+	vk::RenderPass             render_pass;                  // The renderpass description.
+	vk::PipelineLayout         pipeline_layout;              // The pipeline layout for resources.
+	vk::Pipeline               pipeline;                     // The graphics pipeline.
+	vk::DebugReportCallbackEXT debug_report_callback;        // The debug report callback.
+	std::vector<vk::Semaphore> recycled_semaphores;          // A set of semaphores that can be reused.
+	std::vector<FrameData>     per_frame_data;               // A set of per-frame data.
+
+#if defined(VKB_DEBUG) || defined(VKB_VALIDATION_LAYERS)
+	vk::DebugReportCallbackCreateInfoEXT debug_report_callback_create_info;
+#endif
+};
+
+std::unique_ptr<vkb::Application> create_hpp_hello_triangle();
